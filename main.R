f26a2a548236597379035ae61888fd2e826bb30d
hadimashin <hadimashinchi@gmail.com>
2018-10-14 11:43:11 +1100

    Revert "exclusion criteria"
    This reverts commit 7851d711a9ec7492f10b3995aa59ac4a7eaa5121.

--- main.R
+++ main.R
@@ -1,25 +1,16 @@
 
 rm(list=ls(all=TRUE))
 library(xlsx)
-reporDirectory = "C:/repo/COPD/"
-workingDirectoryData = paste(reporDirectory,"Data/", sep = "")
-workingDirectoryCode = paste(reporDirectory,"Code/", sep = "") 
-setwd(workingDirectoryData)
+setwd("C:/Users/home/Desktop/COPD research")
 
-
-#to be done: check the column names, they need to be the same
-dataIsStored = FALSE
+dataIsStored = TRUE
 if(!dataIsStored)
 {
-  #reading the input that contains what columns are to be included
-  inclusionColumnNames = read.xlsx(file = "columnNamesCOPDStudy.xlsx", sheetIndex = 1)
-  columnNamesforInclusion = names(inclusionColumnNames)[which(inclusionColumnNames == TRUE)]
   #reading the data and remove tge data with APACHEDiagnosis != 206
-  fileNames = list.files(pattern="COMET-Extract-APD*", all.files=FALSE,full.names=FALSE)
+  fileNames = list.files(pattern=".xlsx", all.files=FALSE,full.names=FALSE)
   myfunc <- function(x) {
     myData<-read.xlsx(x, sheetIndex = 1)
     myData<-subset(myData, APACHEDiagnosis == 206, select=-c(APACHEDiagnosis)) 
-    myData <- myData[,columnNamesforInclusion[-which(columnNamesforInclusion ==  "APACHEDiagnosis")]]
     return(myData)
   } 
   dataList <- lapply(fileNames, myfunc)
@@ -30,22 +21,27 @@ if(!dataIsStored)
   dataStiched = read.xlsx(file = "COPDStiched.xlsx", sheetIndex = 1)
 }
 
-#There is some vital information missing from this source. This information can be collected from another source
-#adding columns that requires more data collection
-setwd(workingDirectoryCode)
-source("createAmendFile.R")
-requiresAmendment = TRUE
-if(requiresAmendment)
+myData<-dataStiched
+
+
+
+#Return the sig variables for a given output variable
+function(myData,outputVariableName)
 {
-  createAmendFile(dataStiched,workingDirectoryData) #create COPDStichedAmend.xlsx for the user to store the data into
+  pValue = 0.05
+  outputVariableName = "HospitalOutcome"
+  myData[,outputVariableName]<-as.factor(myData[,outputVariableName])
+  inputVariables <- names(myData)[!(names(myData) %in% outputVariableName)]
+  regressionFormula = paste(paste(outputVariableName,"~"),paste(inputVariables,collapse  = "+") ,sep = "")
+  regressionFormula <- as.formula(regressionFormula) 
+  model <- glm("HospitalOutcome~ Sex",data=myData)
+  significantVariables <- summary(model)$coeff < pValue
+  
+  
+  
+  regressionFormula <- as.formula(paste(paste(outputVariableName,"~"),significantVariables,sep = "+")) 
+  model <- glm(regressionFormula,data=mydata)
+  significantVariables <- summary(model)$coeff < pValue
+  return(model)
+  
 }
-
-#removing observations based on the Exclusion criteria of the study protocol
-setwd(workingDirectoryCode)
-source("exclusionCriteria.R")
-includedData = exclusionCriteria(workingDirectoryData)
-
-
-
-
-
