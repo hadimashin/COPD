a08161d29d2268cc684d4434fb46df611f191d8a
hadimashin <hadimashinchi@gmail.com>
2018-10-14 12:00:50 +1100

    code&Data folder, data cleansing with exclusion criteria fun

--- main.R
+++ main.R
@@ -1,7 +1,13 @@
 
 rm(list=ls(all=TRUE))
 library(xlsx)
-setwd("C:/Users/home/Desktop/COPD research")
+
+
+workingDirectory = "C:/repo/COPD/"
+workingDirectoryCode = paste(workingDirectory , "Code/", sep = "")
+workingDirectoryData = paste(workingDirectory , "Data/", sep = "")
+
+setwd(workingDirectoryData)
 
 dataIsStored = TRUE
 if(!dataIsStored)
@@ -23,25 +29,6 @@ if(!dataIsStored)
 
 myData<-dataStiched
 
-
-
-#Return the sig variables for a given output variable
-function(myData,outputVariableName)
-{
-  pValue = 0.05
-  outputVariableName = "HospitalOutcome"
-  myData[,outputVariableName]<-as.factor(myData[,outputVariableName])
-  inputVariables <- names(myData)[!(names(myData) %in% outputVariableName)]
-  regressionFormula = paste(paste(outputVariableName,"~"),paste(inputVariables,collapse  = "+") ,sep = "")
-  regressionFormula <- as.formula(regressionFormula) 
-  model <- glm("HospitalOutcome~ Sex",data=myData)
-  significantVariables <- summary(model)$coeff < pValue
-  
-  
-  
-  regressionFormula <- as.formula(paste(paste(outputVariableName,"~"),significantVariables,sep = "+")) 
-  model <- glm(regressionFormula,data=mydata)
-  significantVariables <- summary(model)$coeff < pValue
-  return(model)
-  
-}
+setwd(workingDirectoryCode)
+source("exclusionCriteria.R")
+includedData <- exclusionCriteria(workingDirectoryData)
